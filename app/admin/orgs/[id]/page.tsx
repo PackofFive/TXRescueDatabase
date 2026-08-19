bd5f92d4e444a0061c2d385c92d8973b31bb1a4f
PackofFive <savinghopeless@gmail.com>
2026-08-19 09:42:26 -0500

    Set runtime to edge for page component

--- app/admin/orgs/[id]/page.tsx
+++ app/admin/orgs/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = "edge";
+
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { CAPABILITY_FIELDS, CAPABILITY_STATUSES, RESOURCE_STATUS_OPTIONS } from "@/lib/constants";
